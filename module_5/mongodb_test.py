d8ae843024725aafd18cd9ec07e959469caeaf0f
Rudolfhunter <jpanzersdroid@gmail.com>
2023-07-20 21:34:36 -0500

    Module_5

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,7 @@
+import pymongo
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.v5ddl1q.mongodb.net/"
+client = MongoClient(url)
+db = client.pytech
+print("-- Pytech COllection List --")
+print(db.list_collection_names())
