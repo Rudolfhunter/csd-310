ab2d20de0c9b7dc986bb1a9ddc98dea862ab217f
Rudolfhunter <jpanzersdroid@gmail.com>
2023-07-22 00:13:40 -0500

    Update

---
+++ module_12/Final.py
@@ -0,0 +1,135 @@
+#code from class- Allows a user to connect to the database
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+    "user": "whatabook_user",
+    "password": "MySQL8IsGreat!",
+    "host": "127.0.0.1",
+    "database": "whatabook",
+    "raise_on_warnings": True
+}
+#db = mysql.connector.connect(**config)
+try:
+    db = mysql.connector.connect(**config)
+    print("\n Database user {} connected to MySQL on host {}".format(config["user"], config["host"], config["database"]))
+    input("\n\n Press any key to conintue....")
+finally: 
+    exit
+#Defining cursor
+cursor = db.cursor()
+
+#Function to login
+
+def login():
+    print("Welcome to WhataBook mobile app!")
+    user_account = input("Do you have a user account? Enter 1 for yes and 2 for no.")
+    if user_account == "1":
+        print("Great!")
+        user_id = input("What is your user ID?")
+        user_id = tuple(user_id)
+        try:
+            cursor.execute(f"Select user_id FROM user WHERE user_id = (%s)", (user_id))
+            print("Account found!")
+        except:
+            print("Account not found")
+            login()
+    elif user_account == "2":
+        print("Please create a free account.")
+        user_email = input("What is your email?")
+        user_first = input("What is your first name?")
+        user_last = input("What is your last name?")
+        cursor.execute(f"INSERT INTO user (first_name, last_name) VALUES(%s, %s)", (user_first, user_last))
+        user_first = list(user_first)
+        cursor.execute(f"SELECT user_id, first_name FROM user where first_name = (%s)", (user_first))
+        test = cursor.fetchall()
+        print("Your user id is: {}".format(test[0]))
+        print("Thank you! Your account has been created!")
+    else:
+        login()
+    return user_id
+#now = login()
+
+#List of books
+def books(now):
+    print("-- List of Books --")
+    try:
+        cursor.execute("SELECT book_id, book_name, details, author FROM book")
+        inventory = cursor.fetchall()
+        for novels in inventory:
+            print("Book ID: {}".format(novels[0]))
+            print("Book Name: {}".format(novels[1]))
+            print("Description: {}".format(novels[2]))
+            print("Author: {}".format(novels[3]))
+            print()
+                
+    except: 
+        print("An error has occured please try again")
+        menu(now)
+
+
+#view store location and hours
+def store():
+    print("-- STORE LOCATION AND HOURS --")
+    try:
+        cursor.execute("SELECT store_id, locale FROM store")
+        locations = cursor.fetchall()
+        for location in locations:
+            print("Store ID: {}".format(location[0]))
+            print("Location: {}".format(location[1]))
+            print("Store Hours: M-S 9-5")
+            print()
+    except:
+        menu(now)
+        
+#View wishlist
+def wishlist(now):
+    now = tuple(now)        
+    cursor.execute(f"SELECT book.book_name from wishlist INNER JOIN book ON wishlist.book_id=book.book_id WHERE user_id = (%s)", (now))
+    wishlist_books = cursor.fetchall()
+    print("-- WISHLIST BOOKS --")
+    try: 
+        for book in wishlist_books:
+            print(f"Book Name: {book}")
+            print()
+            wishlist_add(now)
+    except: 
+        menu(now)
+        
+# add books to wishlist
+def add_wishlist(now):
+    now = tuple(now)
+    user_input = input("Do you want to add a book to your wishlist? Press 1 for yes 2 for no.")
+    if user_input == "1":
+        books()
+        print("Please type the book ID you would like to add to your wishlist")
+        add_book = input()
+        add_book = tuple(add_book)
+        print(now)
+        cursor.execute(f"INSERT INTO wishlist (user_id, book_id) VALUES(%s, %s)", (now, add_book))
+        print("The book has been added to your wishlist!")
+        try: 
+            cursor.execute(f"INSERT INTO wishlist (user_id, book_id) VALUES(%s, %s)", (now, add_book))
+            print("The book has been added to your wishlist!")
+        except:
+            print("The book was unable to be added to your wishlist.")
+            menu(now)
+    elif user_input == "2":
+        now = str(now)
+        menu(now)
+
+#Menu
+def menu(id):
+    print("Press 1 to view of list of books available, press 2 to view your wishlist, press 3 to view store hours and locations.")
+    response = input()
+    if response == "1":
+        books(id)
+    elif response == "2":
+        wishlist(id)
+    elif response == "3":
+        store()
+
+#start program
+menu(login())
+
+#books("1")
